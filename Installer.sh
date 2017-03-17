8c46855fdbf3f0e3e6a9e1d55df259ade4d3b958
Isaac Bankier <isaacbankier@gmail.com>
2017-03-17 19:13:17 +1100

    Added more dependencies.

--- Installer.sh
+++ Installer.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+#install dependencies for debian and arch based systems.
+#Recommended system to use is arch.
 sudo pacman -S imagemagick || sudo apt-get imagemagick
-sudo apt-get install python-pip python-dev || sudo pacman -S python-pip python-dev python-virtualenv
+sudo apt-get install python-pip python-dev || sudo pacman -S python-pip python-dev
+sudo apt-get install tesseract-ocr || sudo pacman tesseract-ocr
 
-pip3 install tensorflow-gpu 
-pip3 install tensorflow
+#install dependencies through pip
+#recommended language is python 3 +
+sudo pip3 install wand
+sudo pip3 install tensorflow-gpu
+sudo pip3 install tensorflow
+sudo pip3 install pytesseract
+sudo pip3 install Pillow
+
+#Unfortionatly one of the developers insists we support python 2 as well
+
+sudo pip3 install wand
+sudo pip3 install tensorflow-gpu
+sudo pip3 install tensorflow
+sudo pip3 install pytesseract
+sudo pip3 install Pillow
