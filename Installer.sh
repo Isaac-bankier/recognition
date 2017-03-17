036b0bc3743469898b00a41d30efbddc51a66d80
Isaac-bankier <isaacbankier@gmail.com>
2017-03-17 17:34:34 +1100

    Delete Installer.sh

--- Installer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S imagemagick || sudo apt-get imagemagick
-sudo apt-get install python-pip python-dev || sudo pacman -S python-pip python-dev python-virtualenv
-
-pip3 install tensorflow-gpu 
-pip3 install tensorflow
