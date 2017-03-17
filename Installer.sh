16bdbe55df5bad041c9768398f97a98584f90d9d
Isaac-bankier <isaacbankier@gmail.com>
2017-03-17 12:19:21 +1100

    Add pip install of tensorflow non gpu.

--- Installer.sh
+++ Installer.sh
@@ -4,3 +4,4 @@ sudo pacman -S imagemagick || sudo apt-get imagemagick
 sudo apt-get install python-pip python-dev || sudo pacman -S python-pip python-dev python-virtualenv
 
 pip3 install tensorflow-gpu 
+pip3 install tensorflow
