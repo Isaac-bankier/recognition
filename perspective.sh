43e8788c59b8a76e95e9f45078f2057b0483375f
isaac bankier <isaacbankier@gmail.com>
2017-04-05 18:51:18 +1000

    Did some work on perspective module.

--- perspective.sh
+++ perspective.sh
@@ -1,1 +1,1 @@
-convert building.jpg -matte -virtual-pixel transparent -distort Perspective '7,40 4,30   4,124 4,123   85,122 100,123   85,2 100,30' building_pers.png
+convert building.jpg -matte -virtual-pixel transparent -distort Perspective '87,131 4,30   74,199 4,123   173,126 100,123   166,210 100,30' building_pers.png
