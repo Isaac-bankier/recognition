bda4af06c7a3769bde650806ab81a2ecbfe6077e
isaac bankier <isaacbankier@gmail.com>
2017-05-16 17:00:05 +1000

    Updated the neural net.

--- neural_net.py
+++ neural_net.py
@@ -47,7 +47,18 @@ class network(object):
             last=i
 
     def feed_forward(self, inputs):
-        pass
+        if len(inputs) != self.n_inputs:
+            return None
 
+        for layer in self.network:
+            f_inputs=[]
+            for n in layer:
+                f_inputs.append(n.feed_forward(inputs))
+            inputs=f_inputs
+        return inputs
+
+    def train(self, data):
+        pass
+        
 n = sigmoid_n(3)
 print(n.feed_forward([1,1,1]))
